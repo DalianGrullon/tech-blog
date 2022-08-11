dc19c14235c488dcc955d38127da252b9d834875
Dalian Grullon <dalian.business@aol.com>
2022-08-11 16:00:45 -0400

    Created Comment model to store comments on posts created by users

---
+++ models/Comment.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+    },
+    user: {
+      type: DataTypes.INTEGER,
+      reference: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: false,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 // make associations
 
-module.exports = { User, Post };
+module.exports = { User, Post, Comment };

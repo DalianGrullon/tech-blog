5e52fb94624fc6f51a458be6ded656721d3979a2
Dalian Grullon <dalian.business@aol.com>
2022-08-13 00:42:46 -0400

    Created a post route with comments defining what is needed. Also updated Post model to include a title for the post

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { Post, User } = require('../models/index');
+
+// post
+
+// update
+
+// delete
+
+module.exports = router;
--- models/Post.js
+++ models/Post.js
@@ -11,6 +11,9 @@ Post.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    title: {
+      type: DataTypes.STRING,
+    },
     content: {
       type: DataTypes.TEXT,
     },

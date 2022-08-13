9b86c7f2e8dd1fb977c78b5be466390f4d40acf2
Dalian Grullon <dalian.business@aol.com>
2022-08-13 00:48:35 -0400

    Updated post-seeds to include title

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,26 +2,32 @@ const { Post } = require('../models/index');
 
 const postData = [
   {
+    title: 'post 1',
     content: 'sample post 1',
     user: 1
   },
   {
+    title: 'post 1 #2',
     content: 'sample post 1',
     user: 1
   },
   {
+    title: 'post 2',
     content: 'sample post 2',
     user: 2
   },
   {
+    title: 'post 3',
     content: 'sample post 3',
     user: 3
   },
   {
+    title: 'post 4',
     content: 'sample post 4',
     user: 4
   },
   {
+    title: 'post 5',
     content: 'sample post 5',
     user: 5
   },
